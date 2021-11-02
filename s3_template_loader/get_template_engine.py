7bcaa09c7d95740fb7199e66d644519db53bf4b9
Benjamin Oertel <benjamin.oertel@gmail.com>
2021-11-02 08:17:23 -0700

    Create get_template_engine.py

---
+++ s3_template_loader/get_template_engine.py
@@ -0,0 +1,21 @@
+def get_template_engine():
+  return {
+      "BACKEND": "django.template.backends.django.DjangoTemplates",
+      "NAME": "s3",
+      "DIRS": [],
+      "APP_DIRS": False,
+      "OPTIONS": {
+          "loaders": [
+              (
+                  "django.template.loaders.cached.Loader",
+                  ["s3_template_loaders.loaders.S3TemplateLoader"],
+              )
+          ],
+          "context_processors": [
+              "django.template.context_processors.debug",
+              "django.template.context_processors.request",
+              "django.contrib.auth.context_processors.auth",
+              "django.contrib.messages.context_processors.messages",
+          ],
+      },
+    }
