d89d0d90c26a0c01b4d1a43abca5dd8c8897ff27
Benjamin Oertel <benjamin.oertel@gmail.com>
2021-11-02 08:51:18 -0700

    Update __init__.py

--- s3_template_loader/__init__.py
+++ s3_template_loader/__init__.py
@@ -1,6 +1,6 @@
 from . import views
 from . import loaders
-from .get_template_settings import get_template_settings
+from get_template_settings import get_template_settings
 
 
 __all__ = ['views', 'loaders', '__version__', 'get_template_settings']
