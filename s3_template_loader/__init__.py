3d5c108b118cc93dd3165d68efcc9262f4ff000a
boertel <ben@comediadesign.com>
2021-11-02 13:39:17 -0700

    no csrf and right version

--- s3_template_loader/__init__.py
+++ s3_template_loader/__init__.py
@@ -5,6 +5,6 @@ from . import utils
 
 __all__ = ['views', 'loaders', '__version__', 'utils']
 
-__version__ = '0.1.13'
+__version__ = '0.1.14'
 
 VERSION = __version__
--- s3_template_loader/views.py
+++ s3_template_loader/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.conf import settings
 from django.template.utils import get_app_template_dirs
+from django.views.decorators.csrf import csrf_exempt
 
 __all__ = ['index', 'version', 'clear_template_cache']
 
@@ -11,8 +12,9 @@ def index(request):
     return render(request, "{}/index.html".format(directory))
 
 def version(request):
-    return JsonResponse({ version: settings.S3_TEMPLATE_LOADER_BUILD_DIRECTORY })
+    return JsonResponse({ "version": settings.S3_TEMPLATE_LOADER_BUILD_DIRECTORY })
 
+@csrf_exempt
 def clear_template_cache(request):
     if request.method != 'DELETE':
         return HttpResponseNotAllowed()
